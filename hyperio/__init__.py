fb8f09162ea098732ca0806c83dc86b364384b65
Mikko Kotila <mailme@mikkokotila.com>
2018-05-04 23:52:43 +0300

    fixed init

--- hyperio/__init__.py
+++ hyperio/__init__.py
@@ -1,3 +1,5 @@
-from hyperio import Hyperio
-from ..utils import utils
-from ..data import data
+from hyperio.main import Hyperio
+from .utils import utils
+from .data import data
+
+__version__ = "0.1"
