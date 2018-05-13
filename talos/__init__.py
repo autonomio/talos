9f5c2875790b48a7c842ea82988093dcab3930ea
Mikko Kotila <mailme@mikkokotila.com>
2018-05-13 16:33:07 +0300

    added plots to ta.
    - astetik plots are now available through ta.plots...

--- talos/__init__.py
+++ talos/__init__.py
@@ -3,6 +3,7 @@ from talos.reporting import Reporting
 from talos.metrics.performance import Performance
 from talos.examples import datasets, models, params
 from talos.utils import save_load
+import astetik as plots
 
 del parameters, model, utils, scan, save_load,
 del Performance, reporting, reducers, metrics, examples
