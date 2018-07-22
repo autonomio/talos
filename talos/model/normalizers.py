bb489f885cce3773b5078f0d4be1670c0474d52f
Matthew Carbone <x94carbone@gmail.com>
2018-07-22 14:58:57 -0400

    Lint/cleanup normalizers.py

--- talos/model/normalizers.py
+++ talos/model/normalizers.py
@@ -1,23 +1,17 @@
-from keras.optimizers import SGD, Adam, Adadelta, Adagrad, Adamax, RMSprop, Nadam
+from keras.optimizers import SGD, Adam, Adadelta, Adagrad, Adamax, RMSprop
+from keras.optimizers import Nadam
 
 
 def lr_normalizer(lr, optimizer):
-
     '''NORMALIZE LEARNING RATE ON DEFAULT 1'''
 
     if optimizer == Adadelta:
-        lr = lr
-    elif optimizer == SGD:
-        lr = lr / 100
-    elif optimizer == Adam:
-        lr = lr / 1000
-    elif optimizer == Adagrad:
-        lr = lr / 100
-    elif optimizer == Adamax:
-        lr = lr / 500
-    elif optimizer == RMSprop:
-        lr = lr / 1000
-    elif optimizer == Nadam:
-        lr = lr / 500
+        pass
+    elif optimizer == SGD or optimizer == Adagrad:
+        lr /= 100.0
+    elif optimizer == Adam or optimizer == RMSprop:
+        lr /= 1000.0
+    elif optimizer == Adamax or optimizer == Nadam:
+        lr /= 500.0
 
     return lr
