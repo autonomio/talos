8a619a7cd77ce05ca40c4f0165aee9c9917bc148
Matthew Carbone <x94carbone@gmail.com>
2018-07-26 20:35:39 -0400

    Final checks

--- test_script.py
+++ test_script.py
@@ -6,7 +6,6 @@ from test.core_tests.test_scan import TestIris, TestCancer, TestLoadDatasets
 if __name__ == '__main__':
 
     # TODO describe what all this does
-
     TestIris().test_scan_iris_explicit_validation_set()
     TestIris().test_scan_iris_explicit_validation_set_force_fail()
     TestIris().test_scan_iris_1()
