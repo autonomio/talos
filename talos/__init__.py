b774c213cdbb8942a1c6c51c75764517258e4036
C0NTRIBUTE <c0L0RC0D3R1976>
2018-10-10 13:22:47 +0300

    - cleaned up __init__.py

--- talos/__init__.py
+++ talos/__init__.py
@@ -1,3 +1,4 @@
+# import commands
 from .scan.Scan import Scan
 from .commands.reporting import Reporting
 from .commands.predict import Predict
@@ -5,7 +6,11 @@ from .commands.deploy import Deploy
 from .commands.evaluate import Evaluate
 from. commands.restore import Restore
 
+# other internal imports
 from .examples import datasets, params
+
+# external append imports
+import sklearn.metrics as performance
 import astetik as plots
 from kerasplotlib import TrainingLog as live
 
