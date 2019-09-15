c8cd5dd655770674fa7dba5a7491c906cddfbfe1
Mikko Kotila <mailme@mikkokotila.com>
2019-09-15 22:18:00 +0300

    removed redundant code from output_layer.py
    There was a part of the code that related with the case where `task` is a list ?!

--- talos/model/output_layer.py
+++ talos/model/output_layer.py
@@ -19,24 +19,4 @@ def output_layer(task, last_activation, y_train, y_val):
         activation = None
         last_neuron = 1
 
-    elif isinstance(task, list):
-        try:
-            # multilabel
-            activation = last_activation
-            last_neuron = y_train.shape[1]
-        except IndexError:
-            uniques = np.unique(np.hstack((y_train, y_val)))
-            # binary
-            if uniques == 2:
-                activation = last_activation
-                last_neuron = 1
-            # multiclass (note this supports only < 10 classes)
-            elif uniques <= 10:
-                activation = last_activation
-                last_neuron = np.unique(np.hstack((y_train, y_val)))
-            # continuous maybe, or too many classes
-            else:
-                activation = None
-                last_neuron = 1
-
     return activation, last_neuron
