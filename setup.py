2de8b68a4aab2e0b02f33f14527e295f8935deba
Mikko Kotila <mailme@mikkokotila.com>
2020-01-25 21:12:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ MAINTAINER_EMAIL = 'mailme@mikkokotila.com'
 URL = 'http://autonom.io'
 LICENSE = 'MIT'
 DOWNLOAD_URL = 'https://github.com/autonomio/talos/'
-VERSION = '0.6.4'
+VERSION = '0.6.5'
 
 try:
     from setuptools import setup
