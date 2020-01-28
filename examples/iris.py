f50cc769956e8a8c5f8d9b03a066a3a8b2d0acba
C0NTRIBUTE <c0L0RC0D3R1976>
2020-01-28 20:54:04 +0200

    "improved bug report template"

--- examples/iris.py
+++ examples/iris.py
@@ -1,29 +1,19 @@
 # first import things as you would usually
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.losses import categorical_crossentropy, logcosh
-from tensorflow.keras.activations import relu, elu, softmax
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.activations import relu, softmax
 
 # import talos
 import talos
 
 # load rthe iris dataset
-x, y = talos.datasets.iris()
+x, y = talos.templates.datasets.iris()
 
 # then define the parameter boundaries
 
-p = {'lr': (2, 10, 30),
-     'first_neuron': [4, 8, 16, 32, 64, 128],
-     'hidden_layers': [2, 3, 4, 5, 6],
-     'batch_size': [2, 3, 4],
-     'epochs': [300],
-     'dropout': (0, 0.40, 10),
-     'weight_regulizer': [None],
-     'emb_output_dims': [None],
-     'optimizer': ['adam', 'nadam'],
-     'losses': [categorical_crossentropy, logcosh],
-     'activation': [relu, elu],
-     'last_activation': [softmax]}
+p = {'first_neuron': [8, 16, 32],
+     'batch_size': [2, 3, 4]}
 
 
 # then define your Keras model
@@ -32,17 +22,17 @@ def iris_model(x_train, y_train, x_val, y_val, params):
     model = Sequential()
     model.add(Dense(params['first_neuron'],
                     input_dim=x_train.shape[1],
-                    activation=params['activation']))
-    model.add(Dropout(params['dropout']))
-    model.add(Dense(y_train.shape[1], activation=params['last_activation']))
+                    activation='relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(y_train.shape[1], activation='softmax'))
 
-    model.compile(optimizer=params['optimizer'],
-                  loss=params['losses'],
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
                   metrics=['acc'])
 
     out = model.fit(x_train, y_train,
                     batch_size=params['batch_size'],
-                    epochs=params['epochs'],
+                    epochs=50,
                     verbose=0,
                     validation_data=[x_val, y_val])
 
@@ -52,6 +42,6 @@ def iris_model(x_train, y_train, x_val, y_val, params):
 # and run the scan
 h = talos.Scan(x, y,
                params=p,
-               experiment_name='first_test',
+               experiment_name='talos-debug',
                model=iris_model,
-               fraction_limit=0.5)
+               round_limit=10)
---
+++ examples/version-check.py
@@ -0,0 +1,8 @@
+import tensorflow
+print('TensorFlow %s' % tensorflow.__version__)
+
+import talos
+print('Talos %s' % talos.__version__)
+
+import sys
+print('Python %s' % sys.version.split()[0])
