368c12f7cb7900bfd07145a3be651b04cbf1d814
bjarnet <bjarnet@bmb.sdu.dk>
2020-05-04 15:02:00 +0200

    Added a Resume function (activated with allow_resume=True).The resume function will be useful in at least the following scenarios:- If a scan cannot be completed within the alloted time- If the kernel crashes unexpectedly

--- talos/scan/scan_prepare.py
+++ talos/scan/scan_prepare.py
@@ -3,9 +3,13 @@ def scan_prepare(self):
     '''Includes all preparation procedures up until starting the first scan
     through scan_run()'''
 
-    from .scan_utils import initialize_log
+    from .scan_utils import initialize_log, initialize_config, initialize_pickle
 
     self._experiment_log = initialize_log(self)
+    if self.allow_resume:
+        self._config_file = initialize_config(self)
+        self._keys_file = self._config_file.replace('yaml','keys.yaml')
+        self._pickle_file = initialize_pickle(self)
 
     # for the case where x_val or y_val is missing when other is present
     self.custom_val_split = False
@@ -20,27 +24,60 @@ def scan_prepare(self):
     self._param_dict_keys = sorted(list(self.params.keys()))
 
     # create the parameter object and move to self
+    from os.path import isfile
+    import yaml
+    import pickle
     from ..parameters.ParamSpace import ParamSpace
-    self.param_object = ParamSpace(params=self.params,
-                                   param_keys=self._param_dict_keys,
-                                   random_method=self.random_method,
-                                   fraction_limit=self.fraction_limit,
-                                   round_limit=self.round_limit,
-                                   time_limit=self.time_limit,
-                                   boolean_limit=self.boolean_limit
-                                   )
+    if self.allow_resume and isfile(self._config_file):
+        with open(self._config_file, 'r') as file_object:
+            self.param_object = yaml.unsafe_load(file_object)
+        # Load metric keys from previous round
+        with open(self._keys_file, 'r') as keys_object:
+            self._all_keys, self._metric_keys, self._val_keys = yaml.unsafe_load(keys_object)
+        # mark that it's not a first round
+        self.first_round = False
 
-    # mark that it's a first round
-    self.first_round = True
+        # Load various stores
+        with open(self._pickle_file, "rb") as p_file:
+            self.stores = pickle.load(p_file)
+        self.round_history = self.stores["round_history"]
+        self.peak_epochs = self.stores["peak_epochs"]
+        self.epoch_entropy = self.stores["epoch_entropy"]
+        self.round_times = self.stores["round_times"]
+        self.result = self.stores["result"]
+        self.saved_models = self.stores["saved_models"]
+        self.saved_weights = self.stores["saved_weights"]
+    else:
+        self.param_object = ParamSpace(params=self.params,
+                                       param_keys=self._param_dict_keys,
+                                       random_method=self.random_method,
+                                       fraction_limit=self.fraction_limit,
+                                       round_limit=self.round_limit,
+                                       time_limit=self.time_limit,
+                                       boolean_limit=self.boolean_limit
+                                       )
+        # mark that it's a first round
+        self.first_round = True
 
-    # create various stores
-    self.round_history = []
-    self.peak_epochs = []
-    self.epoch_entropy = []
-    self.round_times = []
-    self.result = []
-    self.saved_models = []
-    self.saved_weights = []
+        # create various stores
+        self.round_history = []
+        self.peak_epochs = []
+        self.epoch_entropy = []
+        self.round_times = []
+        self.result = []
+        self.saved_models = []
+        self.saved_weights = []
+        if self.allow_resume:
+            # prepare for pickling
+            self.stores = {
+                "round_history": self.round_history,
+                "peak_epochs": self.peak_epochs,
+                "epoch_entropy": self.epoch_entropy,
+                "round_times": self.round_times,
+                "result": self.result,
+                "saved_models": self.saved_models,
+                "saved_weights": self.saved_weights,
+            }
 
     # handle validation split
     from ..utils.validation_split import validation_split
--- talos/scan/scan_run.py
+++ talos/scan/scan_run.py
@@ -3,14 +3,22 @@ def scan_run(self):
     '''The high-level management of the scan procedures
     onwards from preparation. Manages round_run()'''
 
+    import yaml
+    import pickle
+    from os.path import isfile
     from tqdm import tqdm
 
     from .scan_prepare import scan_prepare
     self = scan_prepare(self)
 
     # initiate the progress bar
-    self.pbar = tqdm(total=len(self.param_object.param_index),
-                     disable=self.disable_progress_bar)
+    if self.allow_resume:
+        self.pbar = tqdm(total=max(self.param_object.param_index)+1,
+                         disable=self.disable_progress_bar,
+                         initial=self.param_object.param_index[0])
+    else:
+        self.pbar = tqdm(total=len(self.param_object.param_index),
+                         disable=self.disable_progress_bar)
 
     # the main cycle of the experiment
     while True:
@@ -24,6 +32,17 @@ def scan_run(self):
         # otherwise proceed with next permutation
         from .scan_round import scan_round
         self = scan_round(self)
+        # Dump keys to backup file
+        _keys_file = self._config_file.replace('yaml', 'keys.yaml')
+        if self.allow_resume and not isfile(_keys_file):
+            with open(_keys_file, 'w') as keys_object:
+                yaml.dump((self._all_keys, self._metric_keys, self._val_keys), keys_object)
+        # dump ParamSpace and various stores to backup files
+        if self.allow_resume:
+            with open(self._config_file, 'w') as file_object:
+                yaml.dump(self.param_object, file_object, default_flow_style=False)
+            with open(self._pickle_file, "wb") as p_file:
+                pickle.dump(self.stores, p_file)
         self.pbar.update(1)
 
     # close progress bar before finishing
