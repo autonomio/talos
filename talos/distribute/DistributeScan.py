83fc8e4cb9f55022d8b5a8ea8215b926e376ccda
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-04-10 17:54:11 +0530

    bug fix for writing model

--- talos/distribute/DistributeScan.py
+++ talos/distribute/DistributeScan.py
@@ -126,8 +126,10 @@ class DistributeScan(Scan):
         np.save("tmp/x_data_remote.npy", x)
         np.save("tmp/y_data_remote.npy", y)
 
+        from .distribute_utils import create_function_block
         # get model function as a string
-        model_func = inspect.getsource(model)
+        model_func = inspect.getsourcelines(model)[0]
+        model_func = create_function_block(self, model_func)
 
         self.model_func = model_func
         self.model_name = model.__name__
